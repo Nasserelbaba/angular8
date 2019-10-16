0a251f2dac27b7a55e6cd0fc01bee1f0e6e7e222
Nasserelbaba <nassir993@gmail.com>
2019-10-16 16:12:32 +0300

    .gitignore fix

--- angular/src/app/weatherdata.service.ts
+++ angular/src/app/weatherdata.service.ts
@@ -1,13 +1,13 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import {url} from '../../services/url';
+import {urls} from '../../servicess/url';
 
 @Injectable({
   providedIn: 'root'
 })
 export class WeatherdataService {
 
-  private urll=url;
+  private urll=urls;
   constructor(private http:HttpClient) { }
   getdata(){
     return this.http.get(this.urll.main_url);
